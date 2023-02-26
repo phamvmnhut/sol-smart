23ccd1543516b9cceb3c6ee343498a18ba80f2f4
Phạm Văn Minh Nhựt <phamvmnhut@gmail.com>
2023-02-26 16:54:51 +0700

    error handle

--- contracts/EthereumAccountManager.sol
+++ contracts/EthereumAccountManager.sol
@@ -4,20 +4,24 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract EthereumAccountManager {
-
-    function add(uint256 x, uint256 y) public pure returns (uint256) {
-        return x + y;
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
     }
 
-    function sub(uint256 x, uint256 y) public pure returns (uint256) {
-        return x - y;
+    function sub(uint256 a, uint256 b) public pure returns (uint256) {
+        if (a <= b) {
+            revert("Sub not allow");
+        }
+        return a - b;
     }
 
-    function mul(uint256 x, uint256 y) public pure returns (uint256) {
-        return x * y;
-    }
-
-    function div(uint256 x, uint256 y) public pure returns (uint256) {
-        return x / y;
+    function divide(
+        uint256 _numerator,
+        uint256 _denominator
+    ) public pure returns (uint256) {
+        require(_denominator != 0, "Denominator cannot be zero");
+        return _numerator / _denominator;
     }
 }
--- test/EthereumAccountManager.test.ts
+++ test/EthereumAccountManager.test.ts
@@ -9,64 +9,51 @@ const toWei = (num: number) => ethers.utils.parseEther(num.toString())
 const fromWei = (num: BigNumber) => ethers.utils.formatEther(num);
 
 describe('EthereumAccountManager', function () {
-  let safeMathTest: EthereumAccountManager;
-  let owner: SignerWithAddress;
-  let addr1: SignerWithAddress;
-  let addr2: SignerWithAddress;
+  let accountManager: EthereumAccountManager;
 
   beforeEach(async () => {
-    [owner, addr1, addr2] = await ethers.getSigners();
-    const EthereumAccountManagerFactory = await ethers.getContractFactory('EthereumAccountManager', owner);
-    safeMathTest = (await EthereumAccountManagerFactory.deploy()) as EthereumAccountManager;
+    const accountManagerFactory = await ethers.getContractFactory(
+      "EthereumAccountManager"
+    );
+    accountManager = await accountManagerFactory.deploy();
+    await accountManager.deployed();
   });
 
-  describe('add', () => {
-    it('adds two numbers', async () => {
-      const result = await safeMathTest.add(5, 7);
-      expect(result).to.eq(12);
+  describe("add", () => {
+    it("should add two numbers correctly", async () => {
+      const result = await accountManager.add(1, 2);
+      expect(result).to.equal(3);
     });
 
-    it('throws an error when result overflows', async () => {
-      const maxUint256 = BigNumber.from('2').pow(256).sub(2);
-      const result = safeMathTest.add(maxUint256, 3);
-      await expect(result).to.be.revertedWithPanic("0x11");
-    });
-  });
-
-  describe('sub', () => {
-    it('subtracts two numbers', async () => {
-      const result = await safeMathTest.sub(10, 3);
-      expect(result).to.eq(7);
-    });
-
-    it('throws an error when result underflows', async () => {
-      const result = safeMathTest.sub(5, 7);
-      await expect(result).to.be.revertedWithPanic("0x11");
+    it("should revert when overflow happens", async () => {
+      await expect(accountManager.add(ethers.constants.MaxUint256, 1)).to.be
+        .reverted;
     });
   });
 
-  describe('mul', () => {
-    it('multiplies two numbers', async () => {
-      const result = await safeMathTest.mul(2, 5);
-      expect(result).to.eq(10);
+  describe("sub", () => {
+    it("should subtract two numbers correctly", async () => {
+      const result = await accountManager.sub(10, 2);
+      expect(result).to.equal(8);
     });
 
-    it('throws an error when result overflows', async () => {
-      const maxUint256 = BigNumber.from('2').pow(256).sub(1);
-      const result = safeMathTest.mul(maxUint256, 2);
-      await expect(result).to.be.revertedWithPanic("0x11");
+    it("should revert when subtraction not allowed", async () => {
+      await expect(accountManager.sub(2, 10)).to.be.revertedWith(
+        "Sub not allow"
+      );
     });
   });
 
-  describe('div', () => {
-    it('divides two numbers', async () => {
-      const result = await safeMathTest.div(10, 2);
-      expect(result).to.eq(5);
+  describe("divide", () => {
+    it("should divide two numbers correctly", async () => {
+      const result = await accountManager.divide(10, 2);
+      expect(result).to.equal(5);
     });
 
-    it('throws an error when dividing by zero', async () => {
-      const result = safeMathTest.div(5, 0);
-      await expect(result).to.be.revertedWithPanic("0x12");
+    it("should revert when denominator is zero", async () => {
+      await expect(accountManager.divide(10, 0)).to.be.revertedWith(
+        "Denominator cannot be zero"
+      );
     });
   });
 
